e618755e428a0f87c73045f197cd79b47b1378a0
LinaSeverus <severus@ukr.net>
2021-12-11 15:14:27 +0200

    add comments

--- scripts/script.js
+++ scripts/script.js
@@ -4,15 +4,16 @@
 
 function palindrome(num, count = 0) {
 
-    let str = num.toString();
-    let strReverse = str.split("").reverse().join("");
+    let str = num.toString();                           //преобразуем переданное число num в строку
+    let strReverse = str.split("").reverse().join("");  //Переворачиваем строку str. Через split - разбиваем строку на массив.
+    //Через reverse переворачиваем и join - снова объединяем в строку
 
-    if (str === strReverse) {
+    if (str === strReverse) {                           //Если полученное число(строка) === перевернутому записываем в объект и выходим
         return {
             result: num,
             steps: count,
         };
     }
-    return palindrome(num + +strReverse, ++count);
+    return palindrome(num + +strReverse, ++count);      // Если не равно, то преобразуем strReverse в число и запускаем рекурсию со счетчиком + 1
 }
 console.log(palindrome(549));
